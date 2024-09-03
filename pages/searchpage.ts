c138b28e658f678033620a584457ed71850af03e
Олеся Качалова <olesakacalova@MacBook-Pro-Olesa.local>
2024-09-03 19:10:57 +0300

    SearchLink Element

--- pages/searchpage.ts
+++ pages/searchpage.ts
@@ -1,30 +1,26 @@
-import {expect, Locator} from '@playwright/test'
-import Search from @playwright/test
-export class SearchPage {
-    readonly search:Search;
-    readonly searchLink:Locator ; 
-    readonly searchlink2: Locator;
+import {expect, Locator , Page } from '@playwright/test'
+import { constants } from 'buffer';
 
-    constructor (search:Search){
+export class SearchPage {
+    readonly search:Page;
+    readonly searchLink:Locator ; 
+    readonly searchInput: Locator;
+
+    constructor (search:Page){
         this.search=search;
-        this.searchLink=search.locator('.Heading Link Link--primary Text--subdued u-h8');
-        this.searchlink2=search.locator('.a[data-action="toggle-search"]');
+        this.searchLink=search.locator('.a[data-action="toggle-search"]');
     }    
-        async navigate (){
-            await this.search.goto('https://theconnectedshop.com')
-        }
-        async checkTitle (){
-            await expect(this.search).toHaveTitle('The Connected Shop - Smart Locks, Smart Sensors, Smart Home & Office');
-        
-        }
-        async checkUrl(){
-            await expect (this.search).toHaveURL('https://theconnectedshop.com/');
-        
-        }
-        async checkLogoVisibility(){
-            await expect (this.search.locator).toBeVisible();
-            await expect (this.search.locator).not.toBeVisible();
+     
+        async checkSearchLink(){
+            // await expect(this.searchLink).toBeVisible();
+            await expect(this.searchLink).toBeTruthy;
         };
+        async checkSearchLinkText(expectText:string=('Search')){
+            const actualText = await this.searchLink.innerText();
+            const actualText2 = await this.searchLink.getAttribute('href');
+            await expect(actualText).toBe(expectText);
+    
+        }
     };
 
 
